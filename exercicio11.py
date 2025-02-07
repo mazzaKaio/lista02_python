197bcb453393a9a3bdfc38c44b464f011c030d50
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 20:49:37 -0300

    Create exercicio11.py

---
+++ exercicio11.py
@@ -0,0 +1,7 @@
+# 11 - Escreva um programa qu pergunte o nome do aniversariante, em seguida pergunte a sua idade, acrescente mais 1 e exiba: o nome do aniversariante
+# seguida da mensagem "no próximo aniversário você terá" idade + 1 ano
+
+nomeAniversariante = input("Digite o nome do aniversariante:")
+idadeAniversariante = int(input("Digite a idade do aniversariante:"))
+
+print(nomeAniversariante, "no proximo aniversário você terá", idadeAniversariante + 1, "anos!")
