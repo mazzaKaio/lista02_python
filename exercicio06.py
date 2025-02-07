fb6a81a0aeb6cb32614465140e5c51eb7c4f61a8
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 19:40:31 -0300

    Create exercicio06.py
    Criação e execução do exercício 06

---
+++ exercicio06.py
@@ -0,0 +1,6 @@
+# 06 - Faça um programa que solicite o nome e em seguida o sobrenome do usuário e exiba a mensagem "Olá" + nome digitado + sobrenome digitado
+
+nomeUsuario = input("Digite seu nome: ")
+sobrenomeUsuario = input("Digite seu sobrenome: ")
+
+print("Olá", nomeUsuario, sobrenomeUsuario)
