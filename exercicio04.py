03f78e0b18137a47c9681b6361bdbc79260b9abd
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 19:28:23 -0300

    Create exercicio04.py
    Criação do exercicio 04

---
+++ exercicio04.py
@@ -0,0 +1,8 @@
+# 04 - Faça um programa que calcule um aumento de 15% para um salário de R$750
+
+salario = 750
+aumento = 15
+salarioFinal = salario + (salario * aumento/100)
+
+print("Salário base:", salario)
+print("Com aumento:", salarioFinal)
