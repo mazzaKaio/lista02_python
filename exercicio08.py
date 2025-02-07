a326c0173a4366d522b15a0f2df085cbc83e606a
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 20:25:05 -0300

    Update exercicio08.py

--- exercicio08.py
+++ exercicio08.py
@@ -1,7 +1,7 @@
 # 08 - Escreva um programa que peça ao usuario para inserir dois numeros inteiros, some-os e exiba a mensagem "O total é: [soma dos valores]"
 
-num1 = int(input("Digite um número:"))
-num2 = int(input("Digite mais um número:"))
+num1 = int(input("Digite um número inteiro:"))
+num2 = int(input("Digite mais um número inteiro:"))
 
 somaTotal = num1 + num2
 
