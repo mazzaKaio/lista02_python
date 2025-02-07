651e74b3d324d6dc0ce7280cf2078d55568dd0fe
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 19:36:43 -0300

    Update exercicio05.py
    Pequena atualização na linha 4 do exercicio 05

--- exercicio05.py
+++ exercicio05.py
@@ -1,4 +1,4 @@
 # 05 - Faça um programa que solicite o nome do usuário e exiba a mensagem "Olá" + nome digitado
 
 nomeUsuario = input("Digite seu nome:")
-print("Olá", nomeUsuario, "bem-vindo!")
+print("Olá", nomeUsuario)
