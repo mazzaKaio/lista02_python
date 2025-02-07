86ef76b5f2c79234d5072a005bf37bacfc134974
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 19:52:14 -0300

    Create exercicio07.py

---
+++ exercicio07.py
@@ -0,0 +1,3 @@
+# 07 - Escreva um programa que pergunte "Qual U.C você mais gosta?" e na próxima linha a resposta "E gosto de 'o que você desejar'". Desenvolva usando apenas uma linha
+
+print("Qual U.C você mais gosta?\nGosto de programação de aplicativos")
