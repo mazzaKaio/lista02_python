c61d1f3c8483c81bbd174b33cdec2291536fba1b
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 19:56:26 -0300

    Update exercicio03.py

--- exercicio03.py
+++ exercicio03.py
@@ -1,8 +1,8 @@
 # 03 - Escreva um programa que calcule a soma de três variaáveis e imprima o resultado
 
-num1 = float(input("1º número para soma:"))
-num2 = float(input("2º número para soma:"))
-num3 = float(input("3º número para soma:"))
+num1 = 2
+num2 = 3
+num3 = 5
 
 somaTotal = num1 + num2 + num3
 
