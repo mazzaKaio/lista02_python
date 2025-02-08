244c962a20866a92a8d0ec48e4ecd0728ae50d7d
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 21:04:24 -0300

    Uptade exercicio12.py

--- exercicio12.py
+++ exercicio12.py
@@ -1,3 +1,9 @@
 # 12 - Escreva um programa que pergunte o valor total da conta, em seguida pergunte quantos clientes existem, divida a conta pelos clientes
 # e exiba o quanto cada cliente deve pagar, seguida da mensagem "Cada cliente deve pagar: [?]"
 
+totalConta = float(input("Digite quantos reais(R$) deu a conta: "))
+totalClientes = int(input("Digite em quantos clientes vão ser dividida a conta: "))
+
+divisao = float(totalConta / totalClientes)
+
+print("Cada cliente deverá pagar", divisao, "reais(R$)")
--- exercicio13.py
+++ exercicio13.py
@@ -1,1 +1,2 @@
-# 13 - Escreva um programa 
+# 13 - Escreva um programa que solicite um determinado número de dias, em seguida exiba o quanto estes dias equivalem em horas, minutos e segundos
+
