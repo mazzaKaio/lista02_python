01f779c708c38f8fbc41c1ee881268c49a043889
Kaio Mazza <mazza.kaio12@gmail.com>
2025-02-07 21:08:52 -0300

    Update exercicio13.py

--- exercicio13.py
+++ exercicio13.py
@@ -1,2 +1,12 @@
 # 13 - Escreva um programa que solicite um determinado número de dias, em seguida exiba o quanto estes dias equivalem em horas, minutos e segundos
 
+diasDigitados = int(input("Insira um determinado número de dias:"))
+
+horas = int(diasDigitados * 24)
+minutos = int(diasDigitados * 1440)
+segundos = int(diasDigitados * 86400)
+
+print("Em", diasDigitados, "dias há:")
+print(horas, "horas")
+print(minutos, "minutos")
+print(segundos, "segundos")
